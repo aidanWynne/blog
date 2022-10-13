81e0cb3956a9f11d341cbb766296feae3ffd695c
aidanWynne <aidan.s.wynne@gmail.com.au>
2022-10-14 08:38:25 +1100

    enpty

---
+++ src/pages/ArticleList.js
@@ -0,0 +1,7 @@
+const ArticleList = () => {
+    return (
+        <h1>This is the articleList page</h1>
+    )
+}
+
+export default ArticleList;
---
+++ src/pages/List.js
@@ -0,0 +1,7 @@
+const List = () => {
+    return (
+        <h1>This is the list page</h1>
+    )
+}
+
+export default List;
